13c47f63e10c58ee9a93f70a657f6e2673ecf5eb
gmlwp0910 <shjs6711@gmail.com>
2022-05-27 19:30:55 +0900

    [220527 Error VUE ->React]

---
+++ pages/controller/index.tsx
@@ -0,0 +1,25 @@
+import { Engine } from "@babylonjs/core";
+import { FirstPersonController } from "../../src/units/controller/Controller.component";
+
+
+const ControllerBox = () =>{
+
+
+
+ 
+      
+
+    return (
+        <>
+           
+
+            <div>알아들어라</div>
+          <Engine>
+              
+          </Engine>
+
+        </>
+    )
+}
+
+export default ControllerBox;
